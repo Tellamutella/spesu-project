a6c12c434a3e4f672e1bcfec3bd9730b1419fc21
Edward Lee <eddy2706@gmail.com>
2019-10-11 18:10:15 +0200

    fixed images

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("uploads"));
+app.use(express.static(path.join(__dirname,"uploads")));
 hbs.registerPartials(path.join(__dirname, "views/partials"));
 
 // passport.use(
